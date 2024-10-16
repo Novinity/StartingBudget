ebb4064e313c197acef450dba1ec9185b7a54711
Novinity <novinity63@gmail.com>
2024-10-16 11:22:15 +1000

    1.1.1 - Fixed bug where failing quote would not give owed money

---
+++ Patches/SurfaceNetworkHandlerPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartingBudget.Patches {
+    [HarmonyPatch(typeof(SurfaceNetworkHandler))]
+    public class SurfaceNetworkHandlerPatch {
+        [HarmonyPatch(nameof(SurfaceNetworkHandler.FailedQuota))]
+        [HarmonyPostfix]
+        public static void FailedQuotaPostfix() {
+            if (Plugin.Registered) {
+                Plugin.Logger.LogDebug("Failed quota, unregistering...");
+                Plugin.Registered = false;
+            }
+        }
+    }
+}
