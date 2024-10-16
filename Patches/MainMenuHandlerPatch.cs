0a3a097494ecf4c82f2b7a0e39e80c94dd5d3159
Novinity <novinity63@gmail.com>
2024-10-16 11:09:34 +1000

    1.1.0 - Fixed bug where currency would not be added in new save until game is restarted

---
+++ Patches/MainMenuHandlerPatch.cs
@@ -0,0 +1,18 @@
+﻿using HarmonyLib;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StartingBudget.Patches {
+    [HarmonyPatch(typeof(MainMenuHandler))]
+    public class MainMenuHandlerPatch {
+        [HarmonyPatch(nameof(MainMenuHandler.Awake))]
+        [HarmonyPostfix]
+        private static void Awake() {
+            if (Plugin.Registered) {
+                Plugin.Logger.LogDebug("Returned to main menu, unregistering.");
+                Plugin.Registered = false;
+            }
+        }
+    }
+}
